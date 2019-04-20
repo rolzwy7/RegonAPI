23942f960f967984ba2d4becc1c27a160ad56b4f
rolzwy7 <rolzwy7@gmail.com>
2019-04-20 18:43:47 +0200

    pypi setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("pypi-readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RegonAPI",
+    version="1.0.0",
+    author="Bartosz Nowakowski",
+    author_email="rolzwy7@gmail.com",
+    description="Python 3 API Client for Polish REGON database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rolzwy7/RegonAPI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Database :: Front-Ends",
+        "Environment :: Console",
+        ],
+    install_requires=[
+       "zeep",
+       "beautifulsoup4",
+       ]
+)
